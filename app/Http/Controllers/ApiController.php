426bf317dda425c7b22200580524ef070e97b8de
yd-tak <yudhistiradewanata@teknologiabadikorpora.com>
2024-12-03 17:19:06 +0700

    update apps

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -424,6 +424,7 @@ class ApiController extends Controller {
 
             Item::closeItem($request->item_id);
             $item=Item::with('user:id,seller_uname,subdistrictid','item_bid:id,user_id,bid_amount,bid_price,tipe,created_at','item_payment','category:id,name,image', 'gallery_images:id,image,item_id')->where('id',$request->item_id)->first();
+            // var_dump($item);exit;
 
             if($item==null){
                 throw new \Exception("Item not found");
@@ -2002,10 +2003,10 @@ class ApiController extends Controller {
                     $row->bidstatus='closed';
                     $close_itemids[]=$row->id;
                 }
-                elseif($enddt>$now && $row->bidstatus=='closed'){
-                    $row->bidstatus='open';
-                    $open_itemids[]=$row->id;
-                }
+                // elseif($enddt>$now && $row->bidstatus=='closed'){
+                //     $row->bidstatus='open';
+                //     $open_itemids[]=$row->id;
+                // }
                 $bidHistories[]=$row;
             }
             if(!empty($open_itemids)){
